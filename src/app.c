06baaab7c497cb64906306a79fffc4507288cdee
liam <wonhyuk1994@gmail.com>
2024-11-05 18:13:00 +0900

    깃이그노어 수정 후 Push ( *.hex )

--- src/app.c
+++ src/app.c
@@ -183,13 +183,14 @@ void APP_Tasks ( void )
         //         APP_BleDsadvStart(true);
         //         printf(" - ELSE \r\n");
         //     }
-        // // /* ------------ App 에 있는 코드 --------------*/
+        
+        // /* ------------ App 에 있는 코드 --------------*/
 
 
         case APP_STATE_INIT:
         {
             bool appInitialized = true;
-            //appData.appQueue = xQueueCreate( 10, sizeof(APP_Msg_T) );
+            appData.appQueue = xQueueCreate( 10, sizeof(APP_Msg_T) );
 
             DEVICE_DeepSleepWakeSrc_T wakeSrc;
             DEVICE_GetDeepSleepWakeUpSrc(&wakeSrc);
@@ -207,8 +208,8 @@ void APP_Tasks ( void )
             // }
             switch ( wakeSrc ){
                 case DEVICE_DEEP_SLEEP_WAKE_NONE:{    // 0 초기 상태
-                    printf("0 DEVICE_DEEP_SLEEP_WAKE_NONE DS START : %d \r\n",wakeSrc);
                     RTC_Timer32Start();
+                    printf("0 DEVICE_DEEP_SLEEP_WAKE_NONE DS START : %d \r\n",wakeSrc);
                     DEVICE_EnterDeepSleep(false,0);
                     /*  RB0 ,   RB3 ,   RB5
                         2150    2150    2150*/
@@ -217,22 +218,25 @@ void APP_Tasks ( void )
                     break;
                 } case DEVICE_DEEP_SLEEP_WAKE_INT0:{  // 1 사용자 스위치
                     printf("1 DEVICE_DEEP_SLEEP_WAKE_INT0 DSADV START : %d \r\n",wakeSrc);
-                    APP_BleStackInit();
-                    APP_BleDsadvStart(false);
+                    APP_BleStackInit();             // 
+                    APP_BleDsadvStart(false);       // <- Deep sleep 명령후 프로그램 진행(background)
+                    // DEVICE_EnterDeepSleep(false,0); // <- Deep sleep 프로그램 정지            
                     printf("1 DEVICE_DEEP_SLEEP_WAKE_INT0 DSADV END : %d \r\n",wakeSrc);
                     break;
-                } case DEVICE_DEEP_SLEEP_WAKE_RTC:{   /* 2 RTC 타이머에 의한 Interrupt
-                app_ble_dsadv.c 에 "#define DSADV_INTERVAL    1536" 해당 값에 의해 DEVICE_DEEP_SLEEP_WAKE_RTC Interrupt 발생
-                ex) DSADV_INTERVAL이 1600 일시, 0.625ms x 1600 = 1000ms(1초)
-                + app_ble.c 파일에 있는 Adv interval 값도 바꿔야 함
-                  advParam.intervalMin = 1536;
-                  advParam.intervalMax = 1600;
-                */
-                    printf("2 DEVICE_DEEP_SLEEP_WAKE_RTC DSADV START : %d \r\n",wakeSrc);
-                    APP_BleDsadvStart(true);
-                    printf("2 DEVICE_DEEP_SLEEP_WAKE_RTC DSADV END : %d \r\n",wakeSrc);
-                    break;
-                } case DEVICE_DEEP_SLEEP_WAKE_DSWDT:{
+                } 
+                // case DEVICE_DEEP_SLEEP_WAKE_RTC:{   /* 2 RTC 타이머에 의한 Interrupt
+                // app_ble_dsadv.c 에 "#define DSADV_INTERVAL    1536" 해당 값에 의해 DEVICE_DEEP_SLEEP_WAKE_RTC Interrupt 발생
+                // ex) DSADV_INTERVAL이 1600 일시, 0.625ms x 1600 = 1000ms(1초)
+                // + app_ble.c 파일에 있는 Adv interval 값도 바꿔야 함
+                //   advParam.intervalMin = 1536;
+                //   advParam.intervalMax = 1600;
+                // */
+                //     printf("2 DEVICE_DEEP_SLEEP_WAKE_RTC DSADV START : %d \r\n",wakeSrc);
+                //     APP_BleDsadvStart(true);
+                //     printf("2 DEVICE_DEEP_SLEEP_WAKE_RTC DSADV END : %d \r\n",wakeSrc);
+                //     break;
+                // } 
+                case DEVICE_DEEP_SLEEP_WAKE_DSWDT:{
                     printf("3 DEVICE_DEEP_SLEEP_WAKE_DSWDT : %d \r\n",wakeSrc);
                     break;
                 } case DEVICE_DEEP_SLEEP_WAKE_MCLR:{  /* 4 리셋 스위치  
@@ -259,6 +263,7 @@ void APP_Tasks ( void )
             if (appInitialized)
             {
                 appData.state = APP_STATE_SERVICE_TASKS;
+                printf("------------------ APP_STATE_INIT END ------------------");
             }
             break;
         }
@@ -267,31 +272,35 @@ void APP_Tasks ( void )
             printf(" +++++++++++ \r\n");
             if (OSAL_QUEUE_Receive(&appData.appQueue, &appMsg, OSAL_WAIT_FOREVER))
             {
-
+              printf("11");
                 if(p_appMsg->msgId==APP_MSG_BLE_STACK_EVT)
                 {
                     // Pass BLE Stack Event Message to User Application for handling
                     APP_BleStackEvtHandler((STACK_Event_T *)p_appMsg->msgData);
+                    printf("22");
                 }
                 else if(p_appMsg->msgId==APP_MSG_BLE_STACK_LOG)
                 {
                     // Pass BLE LOG Event Message to User Application for handling
                     APP_BleStackLogHandler((BT_SYS_LogEvent_T *)p_appMsg->msgData);
+                    printf("33");
                 }
 
 
-
+            printf("44");
             }
+            printf("  BREAK");
             break;
         }
+        printf("55");
 
         /* TODO: implement your application state machine.*/
-
 
         /* The default state should never be executed. */
         default:
         {
             /* TODO: Handle error in application's state machine. */
+            printf("66");
             break;
         }
     }
